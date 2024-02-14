afd120038c09f4470c0a4f4f69c7c93bab3c3a2c
Alex-Stone-CGI <alex.stone@cgi.com>
2024-02-14 14:43:06 +0000

    Make it easier to start and run locally

---
+++ cleanBuildTestAndRun.ps1
@@ -0,0 +1,23 @@
+# PowerShell Script: cleanBuildTestAndRun.ps1
+
+$env:MONGO_DB_URI = "mongodb://username:password@host:port/?ssl=true"
+$env:VACCINATIONS_URL = "http://host:port"
+
+Write-Host "Environment Variables:"
+Write-Host "MONGO_DB_URI=$env:MONGO_DB_URI"
+Write-Host "VACCINATIONS_URL=$env:VACCINATIONS_URL"
+
+Write-Host "Navigating to the microservice root directory:" $PSScriptRoot
+Set-Location -Path $PSScriptRoot
+
+Write-Host "Clear any pre-existing generated and compiled classes"
+.\gradlew clean --no-daemon
+
+Write-Host "Generate classes and compile"
+.\gradlew build --no-daemon
+
+Write-Host "Run all tests"
+.\gradlew test --no-daemon
+
+Write-Host "Start the microservice"
+.\gradlew bootRun --no-daemon
