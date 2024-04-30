8e14c706c301d6d2ce488bca48e60dc183d7d379
Alex-Stone-CGI <alex.stone@cgi.com>
2024-04-30 10:39:04 +0100

    Update for OAuth2 server

--- src/main/java/com/cgi/example/petstore/config/SecurityConfiguration.java
+++ src/main/java/com/cgi/example/petstore/config/SecurityConfiguration.java
@@ -20,25 +20,25 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain httpSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(
-                    http -> {
-                      http.requestMatchers(HttpMethod.GET, "/swagger-ui/**")
-                              .permitAll()
-                              .requestMatchers(HttpMethod.GET, "/v3/api-docs/**")
-                              .permitAll()
-                              .requestMatchers(HttpMethod.GET, "/actuator/**")
-                              .permitAll()
-                              .requestMatchers(HttpMethod.GET, "/v3/api-docs.yaml")
-                              .permitAll()
-                              .requestMatchers("/api/v1/pet-store/pets/**")
-                              .authenticated()
-                              .anyRequest()
-                              .denyAll();
-                    })
-            .oauth2ResourceServer(oauth2())
-            .sessionManagement(sessionManagement())
-            .build();
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(
+            http -> {
+              http.requestMatchers(HttpMethod.GET, "/swagger-ui/**")
+                  .permitAll()
+                  .requestMatchers(HttpMethod.GET, "/v3/api-docs/**")
+                  .permitAll()
+                  .requestMatchers(HttpMethod.GET, "/actuator/**")
+                  .permitAll()
+                  .requestMatchers(HttpMethod.GET, "/v3/api-docs.yaml")
+                  .permitAll()
+                  .requestMatchers("/api/v1/pet-store/pets/**")
+                  .authenticated()
+                  .anyRequest()
+                  .denyAll();
+            })
+        .oauth2ResourceServer(oauth2())
+        .sessionManagement(sessionManagement())
+        .build();
   }
 
   private Customizer<OAuth2ResourceServerConfigurer<HttpSecurity>> oauth2() {
@@ -46,14 +46,14 @@ public class SecurityConfiguration {
     entryPoint.setRealmName("Pet Store API Realm");
 
     return oauth2 ->
-            oauth2
-                    .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
-                    .authenticationEntryPoint(entryPoint)
-                    .jwt(Customizer.withDefaults());
+        oauth2
+            .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+            .authenticationEntryPoint(entryPoint)
+            .jwt(Customizer.withDefaults());
   }
 
   private Customizer<SessionManagementConfigurer<HttpSecurity>> sessionManagement() {
     return sessionSecurity ->
-            sessionSecurity.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        sessionSecurity.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
 }
