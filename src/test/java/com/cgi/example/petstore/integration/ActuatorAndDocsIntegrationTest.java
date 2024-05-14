f5379967e6166b8167969c4f64a1906189e3786b
Alex-Stone-CGI <alex.stone@cgi.com>
2024-05-14 12:13:33 +0100

    Remove test which fails if not Git tag is present

--- src/test/java/com/cgi/example/petstore/integration/ActuatorAndDocsIntegrationTest.java
+++ src/test/java/com/cgi/example/petstore/integration/ActuatorAndDocsIntegrationTest.java
@@ -1,7 +1,11 @@
 package com.cgi.example.petstore.integration;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import com.cgi.example.petstore.utils.UriBuilder;
 import com.jayway.jsonpath.JsonPath;
+import java.util.Set;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -10,11 +14,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.Set;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 @Tag("integration")
 class ActuatorAndDocsIntegrationTest extends BaseIntegrationTest {
